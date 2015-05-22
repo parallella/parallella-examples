1f84cb1cea148dc4f29d0daa40dc4504ff252bb7
Ola Jeppsson <ola@adapteva.com>
2015-05-22 13:02:50 +0000

    Add grayscale_to_jpeg function declaration to header file
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- demo.h
+++ demo.h
@@ -7,6 +7,7 @@
 float *jpeg_file_to_grayscale(char *path, int *width, int *height);
 float *jpeg_to_grayscale(void *jpeg, size_t jpeg_size, int *width, int *height);
 bool grayscale_to_jpeg_file(float *bitmap, int width, int height, char *path);
+void *grayscale_to_jpeg(float *bitmap, int width, int height, unsigned long *jpeg_size);
 
 /* FFT implementation hooks */
 bool fftimpl_init();
